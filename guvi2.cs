9a1c7f3094b229d3d07f26d783d68d29c470b8e1
Ankit malik <stalemalik@users.noreply.github.com>
2017-03-09 18:45:11 +0530

    Create guvi2.cs

---
+++ guvi2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+			
+public class Program
+{
+  public static void Main()
+  {
+    //Console.WriteLine("Hello C#");
+    int n=Convert.ToInt32(Console.ReadLine());
+    
+    if(n%2==0)
+      Console.WriteLine("even");
+      else
+      Console.WriteLine("odd");
+
+  }
+}
