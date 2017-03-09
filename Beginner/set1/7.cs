01dc9bcbfa7ea2f048b7ea7010b4644948c4cc52
Ankit malik <stalemalik@users.noreply.github.com>
2017-03-10 02:33:00 +0530

    Create 7.cs

---
+++ Beginner/set1/7.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Numerics;
+			
+public class Program
+{
+  public static void Main()
+  {
+    //Console.WriteLine("Hello C#");
+    for(int i=0;i<5;i++)
+    {
+      Console.WriteLine("Hello");
+    }
+    
+  }
+}
