6b44cc5c69d6377a5d2945d4378c87bb453cbdef
Ankit malik <stalemalik@users.noreply.github.com>
2017-03-10 02:36:00 +0530

    Create 8.cs

---
+++ Beginner/set1/8.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Numerics;
+			
+public class Program
+{
+  public static void Main()
+  {
+    //Console.WriteLine("Hello C#");
+    int a=Convert.ToInt32(Console.ReadLine());
+    
+    Console.WriteLine((a*(a+1))/2);
+    
+  }
+}
